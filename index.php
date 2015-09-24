f6af570bb7e9bd7221619178e0e2658a2eacb0a4
bradenhan <bradenhan@gmail.com>
2015-09-24 21:26:21 +0800

    新增首页

---
+++ index.php
@@ -0,0 +1,203 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> 智能运维平台
+		</title>
+		<script>
+			if (/Android (\d+\.\d+)/.test(navigator.userAgent)){if (parseFloat(RegExp.$1) > 2.3){var phoneScale = parseInt(window.screen.width) / 640;document.write('<meta name="viewport" content="width=640,minimum-scale=' + phoneScale + ',maximum-scale=' + phoneScale + ',target-densitydpi=device-dpi" />');}else{document.write('<meta name="viewport" content="width=640,target-densitydpi=device-dpi" />');}}else{document.write('<meta name="viewport" content="width=640,user-scalable=no,target-densitydpi=device-dpi" />');}
+		</script>
+		<base target="_blank" />
+		<style>table {
+	border-spacing: 0;
+	border-collapse: collapse;
+	font-size: 14px;
+	line-height: 18px;
+}
+table td {
+	-webkit-box-sizing: border-box;
+	box-sizing: border-box;
+	height: 20px;
+	padding: 10px 50px;
+	border: 1px solid #ccc;
+}
+table tr:nth-child(2n+1) td {
+	background: #ddd;
+}</style>
+	</head>
+	<body>
+		<table>
+			<tbody>
+				<tr>
+					<th colspan="3" style="height: 46px; padding: 0 10px; border: 1px solid #085f91; background-color: #1078b5; color: #fff; text-align: left;"> 
+						智能运维平台
+					</th> 
+				</tr>
+				<tr>
+					<th colspan="4" style="height: 40px;">微信网站</th>
+				</tr>
+				<tr>
+					<td>
+						1
+					</td>
+					<td>
+						应急任务
+					</td>
+					<td>
+						<a href="intelligentOperation/html/wechat01emergencyTask.html">
+							wechat01emergencyTask.html
+						</a>
+					</td>  
+				</tr>
+				<tr>
+					<td>
+						2
+					</td>
+					<td>
+						抢单列表页
+					</td>
+					<td>
+						<a href="intelligentOperation/html/wechat02-grabOrder.html">
+							wechat02-grabOrder.html
+						</a>
+					</td>  
+				</tr>
+				<tr>
+					<td>
+						3
+					</td>
+					<td>
+						任务详情
+					</td>
+					<td>
+						<a href="intelligentOperation/html/wechat03-grabOrderDetail.html">
+							wechat03-grabOrderDetail.html
+						</a>
+					</td>  
+				</tr>
+				<tr>
+					<td>
+						4
+					</td>
+					<td>
+						填写回单--任务详情
+					</td>
+					<td>
+						<a href="intelligentOperation/html/wechat04-receiptOrder.html">
+							wechat04-receiptOrder.html
+						</a>
+					</td>  
+				</tr>
+				<tr>
+					<td>
+						5
+					</td>
+					<td>
+						填写回单--维护服务报告
+					</td>
+					<td>
+						<a href="intelligentOperation/html/wechat05-fillReceiptOrder.html">
+							wechat05-fillReceiptOrder.html
+						</a>
+					</td>  
+				</tr>
+				<tr>
+					<td>
+						6
+					</td>
+					<td>
+						填写回单--维修历史记录
+					</td>
+					<td>
+						<a href="intelligentOperation/html/wechat06-repairHistoryList.html">
+							wechat06-repairHistoryList.html
+						</a>
+					</td>  
+				</tr>abandon
+				<tr>
+					<td>
+						7
+					</td>
+					<td>
+						退单
+					</td>
+					<td>
+						<a href="intelligentOperation/html/wechat07-abandonOrder.html">
+							wechat07-abandonOrder.html
+						</a>
+					</td>  
+				</tr>
+				<tr>
+					<td>
+						8
+					</td>
+					<td>
+						待评价回单列表
+					</td>
+					<td>
+						<a href="intelligentOperation/html/wechat08-evaluationOrderList.html">
+							wechat08-evaluationOrderList.html
+						</a>
+					</td>  
+				</tr>
+				<tr>
+					<td>
+						9
+					</td>
+					<td>
+						待评价回单详情
+					</td>
+					<td>
+						<a href="intelligentOperation/html/wechat09-evaluationOrderDetail.html">
+							wechat09-evaluationOrderDetail.html
+						</a>
+					</td>  
+				</tr>
+				<tr>
+					<td>
+						10
+					</td>
+					<td>
+						交通路线
+					</td>
+					<td>
+						<a href="intelligentOperation/html/wechat10-trafficRoutes.html">
+							wechat10-trafficRoutes.html
+						</a>
+					</td>  
+				</tr>
+				<tr>
+					<th colspan="4" style="height: 40px;">other</th>
+				</tr> 				 
+				<tr>
+					<td>
+						1
+					</td>
+					<td>
+						登录
+					</td>
+					<td>
+						<a href="intelligentOperation/html/other01-login.html">
+							other01-login.html
+						</a>
+					</td>  
+				</tr>  
+				<tr>
+					<td>
+						2
+					</td>
+					<td>
+						 
+					</td>
+					<td>
+						<a href="intelligentOperation/html/">
+							 
+						</a>
+					</td>  
+				</tr>  
+		</table>
+		<address>
+		</address>
+	</body>
+</html>
